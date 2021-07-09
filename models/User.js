6ca5c56ed6a4be0c7ee53c817144f8e7eacac0bd
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-07-08 17:44:46 -0700

    fixed signup js

--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ User.init(
         password: {
             type: DataTypes.STRING,
             allowNull: false,
-        }
+        } 
     },
     {
         hooks: {
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,8 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const username = document.querySelector("#signupUsername");
-    const password = document.querySelector("#signupPassword");
+    const username = document.querySelector("#signupUsername").value.trim();
+    const password = document.querySelector("#signupPassword").value.trim();
 
     if (username && password) {
         const response = await fetch("api/users", {
